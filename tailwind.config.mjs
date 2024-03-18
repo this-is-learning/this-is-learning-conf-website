6cebb89c08985aea18c932ae9be5e973674cbe6c
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-03-18 17:22:24 +0100

    feat: use figma styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	daisyui: {
+		theme: ["light"]
+	},
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'freeSans': ['FreeSans Bold', 'sans-serif'],
+			},
+			dropShadow: {
+				'til': '2px 2px 0 #21005D',
+			},
+		},
 	},
 	plugins: [require("daisyui")],
 }
