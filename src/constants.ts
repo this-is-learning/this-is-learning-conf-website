d2872f85a4e3056dcd0e24674e774258cd6aa1d2
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-03-21 19:42:48 +0100

    fix: navbar links

--- src/constants.ts
+++ src/constants.ts
@@ -3,13 +3,13 @@ import type {INavigationLink} from "./types/navigation-link.type.ts";
 export const SITE_TITLE = "TIL-Conf";
 
 export const NAVIGATION_LINKS: Array<INavigationLink> = [
-    {title: 'Agenda', href: '/agenda'},
-    {title: 'Speakers', href: '/speakers'},
-    {title: 'Sponsors', href: '/sponsors'},
-    {title: 'About', href: '/about'},
+    // {title: 'Agenda', href: '/agenda'},
+    // {title: 'Speakers', href: '/speakers'},
+    // {title: 'Sponsors', href: '/sponsors'},
+    // {title: 'About', href: '/about'},
+    {title: 'CFP', href: 'https://docs.google.com/forms/d/1XyoW2doQvHg51Vh29iEgE6Q6RTX8zbmxJfOg9zAZQmM/viewform?edit_requested=true'},
     {title: 'CoC', href: '/coc'},
-    {title: 'CFP', href: '/cfp'},
-    {title: 'Venue', href: '/venue'},
+    // {title: 'Venue', href: '/venue'},
 ];
 
 export const SOCIAL_LINKS = {
